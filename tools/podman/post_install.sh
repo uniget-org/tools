20a3f152132e132db87c6a600a782a18a5a63761
Nicholas Dille <nicholas.dille@mailbox.org>
2023-09-12 15:13:28 +0200

    Fix prerequisites for podman

--- tools/podman/post_install.sh
+++ tools/podman/post_install.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 set -o errexit
 
+mkdir -p /etc/containers
+ln --symbolic --force "${target}/etc/containers/registries.conf" "/etc/containers/"
+ln --symbolic --force "${target}/etc/containers/registries.json" "/etc/containers/"
+
 echo "Install systemd unit"
 cat "${target}/etc/systemd/system/podman.service" \
 | sed "s|ExecStart=/usr/local/bin/podman|ExecStart=${target}/bin/podman|" \
