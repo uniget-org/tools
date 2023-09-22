06cd485100452216fa5260299d2a745e7a8f7565
Nicholas Dille <nicholas.dille@mailbox.org>
2023-09-22 08:47:18 +0200

    Re-introduced priorities to limit work for Renvoate

--- scripts/renovate.sh
+++ scripts/renovate.sh
@@ -45,12 +45,37 @@ jq '
                     {
                         "matchFiles": [ "^tools/" + .name + "/manifest.yaml$" ],
                         "matchPackageNames": [ .renovate.package ],
-                        "ignoreUnstable": (.renovate.allowPrereleases // false)
+                        "ignoreUnstable": (.renovate.allowPrereleases // false),
+                        "priority": .renovate.priority
                     }
 
                 else
-                    empty
+                    {
+                        "matchFiles": [ "^tools/" + .name + "/manifest.yaml$" ],
+                        "matchPackageNames": [ .renovate.package ],
+                        "priority": .renovate.priority
+                    }
                 end
+                |
+                if .priority == "high" then
+                    .schedule = [ "* 0,8,16 * * *" ]
+                else
+                    .
+                end
+                |
+                if .priority == "medium" then
+                    .schedule = [ "* 4,20 * * *" ]
+                else
+                    .
+                end
+                |
+                if .priority == "low" or .priority == null or .priority == "" then
+                    .schedule = [ "* 12 * * *" ]
+                else
+                    .
+                end
+                |
+                del(.priority)
 
             else
                 empty
