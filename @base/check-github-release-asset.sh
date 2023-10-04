ad78d01eff53cbd14094f59ee118b4ea213fb0cf
Nicholas Dille <nicholas.dille@mailbox.org>
2023-10-04 12:49:28 +0200

    Added checks down download and clone

--- @base/check-github-release-asset.sh
+++ @base/check-github-release-asset.sh
@@ -24,7 +24,7 @@ function check-github-release-asset() {
 
     local url="https://github.com/${repo}/releases/download/${version}/${asset}"
     echo "### Checking ${repo} ${version} ${asset}"
-    if curl --silent --location --fail --head --url "${url}"; then
+    if curl --silent --show-error --location --fail --head --url "${url}"; then
         echo "    Found :-)"
         return
     fi
@@ -34,7 +34,7 @@ function check-github-release-asset() {
     echo "### Fetching release assets for ${repo} ${version}"
     local id
     local url="https://api.github.com/repos/${repo}/releases/tags/${version}"
-    id="$(curl --silent --location "${url}" | jq --raw-output '.id')"
+    id="$(curl --silent --show-error --location "${url}" | jq --raw-output '.id')"
     if test -z "${id}" || test "${id}" == "null"; then
         echo "    ERROR: Failed to fetch release id for ${repo} ${version}"
         echo "           at ${url}"
@@ -44,7 +44,7 @@ function check-github-release-asset() {
 
     echo "### Fetching release assets for ${repo} ${version} with id ${id}"
     local url="https://api.github.com/repos/${repo}/releases/${id}/assets"
-    if ! curl --silent --location --fail "${url}" | jq --raw-output '.[].name'; then
+    if ! curl --silent --show-error --location --fail "${url}" | jq --raw-output '.[].name'; then
         echo "    ERROR: Failed to fetch release assets for ${repo} ${version}"
         echo "           at ${url}"
         return 1
