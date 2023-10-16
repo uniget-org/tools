24604a73d1d3a6d5b05afd5a72711e2240598f60
Nicholas Dille <nicholas.dille@mailbox.org>
2023-10-16 23:38:38 +0200

    Updated sbom/bov/sarif/csv

--- make/sbom.mk
+++ make/sbom.mk
@@ -42,12 +42,45 @@ $(addsuffix /sbom.json,$(ALL_TOOLS)):$(TOOLS_DIR)/%/sbom.json: \
 		$(TOOLS_DIR)/%/manifest.json \
 		$(TOOLS_DIR)/%/Dockerfile \
 		; $(info $(M) Creating sbom for $*...)
-	@set -o errexit; \
-	docker buildx imagetools inspect $(REGISTRY)/$(REPOSITORY_PREFIX)$*:$(DOCKER_TAG) --format "{{ json .SBOM }}" \
-	| jq --arg arch $(ALT_ARCH) 'if .SPDX == null then ."linux/\($$arch)".SPDX else .SPDX end' \
-	| syft convert - --output cyclonedx \
-	>$(TOOLS_DIR)/$*/sbom.json; \
-	test -s $(TOOLS_DIR)/$*/sbom.json || rm $(TOOLS_DIR)/$*/sbom.json
+	@syft packages $(REGISTRY)/$(REPOSITORY_PREFIX)$*:$(DOCKER_TAG) --output cyclonedx-json=$(TOOLS_DIR)/$*/sbom.json
+
+.PHONY:
+bov: \
+		$(addsuffix /bov.json,$(TOOLS))
+
+.PHONY:
+$(addsuffix --bov,$(ALL_TOOLS_RAW)):%--bov: \
+		$(TOOLS_DIR)/%/bov.json
+
+$(addsuffix /bov.json,$(ALL_TOOLS)):$(TOOLS_DIR)/%/bov.json: \
+		$(HELPER)/var/lib/uniget/manifests/grype.json \
+		$(TOOLS_DIR)/%/sbom.json \
+		; $(info $(M) Creating bov for $*...)
+	@grype sbom:$(TOOLS_DIR)/$*/sbom.json --file $(TOOLS_DIR)/$*/bov.json --output cyclonedx-json
+
+.PHONY:
+sarif: \
+		$(addsuffix /sarif.json,$(TOOLS))
+
+.PHONY:
+$(addsuffix --sarif,$(ALL_TOOLS_RAW)):%--sarif: \
+		$(TOOLS_DIR)/%/sarif.json
+
+$(addsuffix /sarif.json,$(ALL_TOOLS)):$(TOOLS_DIR)/%/sarif.json: \
+		$(HELPER)/var/lib/uniget/manifests/grype.json \
+		$(TOOLS_DIR)/%/bov.json \
+		; $(info $(M) Creating sarif for $*...)
+	@grype sbom:$(TOOLS_DIR)/$*/bov.json --file $(TOOLS_DIR)/$*/sarif.json --output sarif
+
+# make report | column --separator ';' --table --table-columns Tool,ID,purl,Score
+.PHONY:
+report: \
+		$(addsuffix --report,$(TOOLS_RAW))
+
+.PHONY:
+$(addsuffix --report,$(ALL_TOOLS_RAW)):%--report: \
+		$(TOOLS_DIR)/%/bov.json
+	@jq --raw-output --arg tool "$*" '.vulnerabilities[] | "\($$tool);\(.id);\(.affects[].ref);\(.ratings[] | select(.method == "CVSSv31") | .score)"' $(TOOLS_DIR)/$*/bov.json
 
 .PHONY:
 attest: \
