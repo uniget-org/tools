c3130f29c59eeb513d9056984d3e04009008b0aa
Nicholas Dille <nicholas.dille@mailbox.org>
2023-10-23 13:11:42 +0200

    Added support for PRs from docker-setup #59

--- scripts/stats.sh
+++ scripts/stats.sh
@@ -1,21 +1,30 @@
 #!/bin/bash
 set -o errexit -o pipefail
 
+function process_prs() {
+    jq '
+        [
+            .[] |
+            {
+                "number": "\(.base.repo.full_name)#\(.number)",
+                "title": .title,
+                "createdAt": (.created_at | fromdateiso8601 | strftime("%Y-%m-%d")),
+                "mergedAt": .merged_at
+            }
+        ]
+    ' \
+    | jq 'group_by(.createdAtDay)' \
+    | jq ' [ .[] | { "date": .[0].createdAtDay, "count": (. | length) } ]'
+}
+
 if ! test -f prs.json; then
     gh api --paginate "repos/uniget-org/tools/pulls?state=closed" >prs.json
 fi
+if ! test -f prs-old.json; then
+    gh api --paginate "repos/nicholasdille/docker-setup/pulls?state=closed" >prs-old.json
+fi
+cat prs-old.json prs.json \
+| process_prs \
+>plot.json
 
-cat pors.json \
-| jq '
-    [
-        .[] |
-        {
-            "number": .number,
-            "title": .title,
-            "createdAt": (.created_at | fromdateiso8601 | strftime("%Y-%m-%d")),
-            "mergedAt": .merged_at
-        }
-    ]
-' \
-| jq 'group_by(.createdAtDay)' \
-| jq ' [ .[] | { "date": .[0].createdAtDay, "count": (. | length) } ]'
+mlr --j2x --from plot.json stats1 -a min,mean,max -f count
