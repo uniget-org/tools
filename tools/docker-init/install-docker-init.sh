4355f3d5d4311d7cae4b06745511d6fd833be2c6
Nicholas Dille <nicholas.dille@mailbox.org>
2024-02-05 17:21:19 +0100

    Added Docker Desktop

---
+++ tools/docker-init/install-docker-init.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -o errexit -o pipefail
+
+target="${1:-/usr/local}"
+version="4.27.1"
+build="136059"
+
+TEMP_DIR="$(mktemp -d)"
+cd "${TEMP_DIR}"
+trap "rm -rf ${TEMP_DIR}" EXIT
+
+curl --location --fail --remote-name "https://desktop.docker.com/linux/main/amd64/${build}/docker-desktop-${version}-amd64.deb"
+ar -x "docker-desktop-${version}-amd64.deb" data.tar.xz
+rm "docker-desktop-${version}-amd64.deb"
+
+mkdir -p "${target}/libexec/docker/cli-plugins"
+tar --extract --xz --file data.tar.xz --directory "${target}/libexec/" --strip-components=3 --no-same-owner \
+    ./usr/lib/docker/cli-plugins/docker-init
