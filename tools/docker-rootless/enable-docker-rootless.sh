7bcee77e727788f2f386de040231cd68d050bb6d
Nicholas Dille <nicholas.dille@mailbox.org>
2024-06-07 12:16:08 +0200

    More fixes

--- tools/docker-rootless/enable-docker-rootless.sh
+++ tools/docker-rootless/enable-docker-rootless.sh
@@ -19,20 +19,43 @@ if test -z "${UNIGET_TARGET}"; then
     UNIGET_TARGET="$( dirname $( dirname $( readlink -f $0 ) ) )"
 fi
 
+mkdir -p \
+    "${HOME}/.config/systemd/user" \
+    "${HOME}/.cache/dockerd-rootless"
 cat "${UNIGET_TARGET}/state/uniget/contrib/docker-rootless/docker.service" \
-| sed "s/\${HOME}/${HOME}/" \
+| sed "s|\${HOME}|${HOME}|g" \
 >"${HOME}/.config/systemd/user/docker.service"
 systemctl --user daemon-reload
-systemctl --user start docker
+systemctl --user restart docker
 
 # Background knowledge:
 #echo -n rootless | sha256sum
 #12b961af5feb3e9d39f93b2cefb9a1a944f18d02cca0cac2f04f5a982240605f  -
 #cat .docker/contexts/meta/12b961af5feb3e9d39f93b2cefb9a1a944f18d02cca0cac2f04f5a982240605f/meta.json
 #{"Name":"rootless","Metadata":{"Description":"Rootless Docker"},"Endpoints":{"docker":{"Host":"unix:///home/rootless/.cache/dockerd-rootless/docker.sock","SkipTLSVerify":false}}}
-docker context create rootless \
-    --description "Rootless Docker" \
-    --docker "host=unix://${HOME}/.cache/dockerd-rootless/docker.sock"
+if ! docker context ls | grep -q ^rootless; then
+    docker context create rootless \
+        --description "Rootless Docker" \
+        --docker "host=unix://${HOME}/.cache/dockerd-rootless/docker.sock"
+else
+    docker context update rootless \
+        --description "Rootless Docker" \
+        --docker "host=unix://${HOME}/.cache/dockerd-rootless/docker.sock"
+fi
 #cat .docker/config.json
 #{"currentContext": "rootless"}
 docker context use rootless
+
+echo
+echo "🔌For publishing privileged ports:"
+echo "    1. Call: sudo setcap cap_net_bind_service=ep $(which rootlesskit)"
+echo "    2. Restart the Docker service: systemctl --user restart docker"
+
+echo
+echo "🚧For resource control:"
+echo "    1. Call: sudo cp ${UNIGET_TARGET}/state/uniget/contrib/docker-rootless/delegate.conf /etc/systemd/system/user@.service.d/delegate.conf"
+echo "    2. Restart the Docker service: systemctl --user restart docker"
+
+echo
+echo "🚀If you want Docker to persist:"
+echo "    Call: sudo loginctl enable-linger ${USER}"
