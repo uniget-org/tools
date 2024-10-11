ca3e1c7541e13127ddcb68281c5446f71fee61bc
Nicholas Dille <nicholas.dille@mailbox.org>
2024-10-11 14:42:49 +0200

    Check dependencies

---
+++ scripts/update-build-deps.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+set -o errexit
+
+grep -l "^FROM ghcr.io/uniget-org/tools/" tools/*/Dockerfile.template \
+| cut -d/ -f2 \
+| while read -r TOOL; do
+    make tools/${TOOL}/manifest.json
+    DEPS="$(
+        grep "^FROM ghcr.io/uniget-org/tools/" tools/${TOOL}/Dockerfile.template \
+        | cut -d' ' -f2 \
+        | cut -d: -f1 \
+        | cut -d/ -f4 \
+        | xargs echo -n
+    )"
+    
+    if test -n "${DEPS}"; then
+        yq --inplace '.build_dependencies = []' tools/${TOOL}/manifest.yaml
+
+        for DEP in $DEPS; do
+            DEP=${DEP} yq --inplace '.build_dependencies += env(DEP)' tools/${TOOL}/manifest.yaml
+        done
+
+        sed -i -E 's/^\s{2}-/-/' tools/${TOOL}/manifest.yaml
+    fi
+done
