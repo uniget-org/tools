80d6f0788e0769546f590ec0070f30b1ee412869
Nicholas Dille <nicholas.dille@mailbox.org>
2024-10-21 11:31:22 +0200

    Improved test for shiv

--- tools/shiv/test.sh
+++ tools/shiv/test.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-: "${VERSION:=latest}"
+if test -z "${name}" || test -z "${version}"; then
+    echo "Usage: name=... version=... $0"
+    exit 1
+fi
 
 container="$(mktemp --dry-run | cut -d. -f2)"
 
@@ -10,8 +13,10 @@ function cleanup() {
 }
 trap cleanup EXIT
 
+echo "Testing ${name} ${version}"
+
 echo "### Creating container ${container}"
-docker run \
+if ! docker run \
     --name "${container}" \
     --detach \
     --privileged \
@@ -20,12 +25,27 @@ docker run \
     --pull always \
     ghcr.io/uniget-org/cli:latest \
         sleep infinity
+then
+    echo "### Failed to create container"
+    exit 1
+fi
         
-time docker exec --interactive "${container}" bash <<EOF
+echo "### Executing test"
+if ! time docker exec --interactive --env name --env version "${container}" bash -e <<"EOF"
 uniget --version
+
+uniget update
+uniget install jq
+mv /var/cache/uniget/metadata.json /var/cache/uniget/metadata.json.bak
+jq --arg name "${name}" --arg version "${version}" '(.tools[] | select(.name == "\($name)") | .version) |= "\($version)"' /var/cache/uniget/metadata.json.bak > /var/cache/uniget/metadata.json
+SHIV_VERSION="$( jq -r --arg name "${name}" '.tools[] | select(.name == "\($name)") | .version' /var/cache/uniget/metadata.json )"
 
 uniget install shiv
 shiv --version
 shiv --output-file=/usr/local/bin/semgrep --console-script=semgrep semgrep
 semgrep --version
 EOF
+then
+    echo "### Failed to complete test"
+    exit 1
+fi
