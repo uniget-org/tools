9009e920028f53e59964b55bf751dfa77b3ae524
Nicholas Dille <nicholas.dille@mailbox.org>
2024-10-21 09:59:31 +0200

    Now includes usage for arm64

--- scripts/usage.sh
+++ scripts/usage.sh
@@ -3,18 +3,28 @@
 : "${REGISTRY:=ghcr.io}"
 : "${OWNER:=uniget-org}"
 : "${PROJECT:=tools}"
-: "${VERSION:=main}"
+: "${VERSION:=latest}"
 
 SUM=0
 (
     for TOOL in $@; do
-        SIZE="$(
+        SIZE_AMD64="$(
             regctl manifest get ${REGISTRY}/${OWNER}/${PROJECT}/${TOOL}:${VERSION} --platform linux/amd64 --format raw-body \
             | jq -r '.layers[].size' \
             | paste -sd+ \
             | bc
         )"
-        SUM=$(( SUM + SIZE ))
+        SUM=$(( SUM + SIZE_AMD64 ))
+
+        SIZE_ARM64="$(
+            regctl manifest get ${REGISTRY}/${OWNER}/${PROJECT}/${TOOL}:${VERSION} --platform linux/arm64 --format raw-body \
+            | jq -r '.layers[].size' \
+            | paste -sd+ \
+            | bc
+        )"
+        SUM=$(( SUM + SIZE_ARM64 ))
+
+        SIZE=$(( SIZE_AMD64 + SIZE_ARM64 ))
 
         SIZE_HUMAN="$( echo "${SIZE}" | numfmt --to=iec --format=%7.1f )"
         echo "${SIZE_HUMAN} ${TOOL}"
