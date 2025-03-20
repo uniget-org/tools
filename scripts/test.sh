aad13d14be2d73538773e983a31fa28f6688b4ef
Nicholas Dille <nicholas.dille@mailbox.org>
2025-03-20 15:44:56 +0100

    Fixed version check

--- scripts/test.sh
+++ scripts/test.sh
@@ -5,22 +5,19 @@ if test -z "${TOOL}"; then
     echo "TOOL is not set"
     exit 1
 fi
-if test -z "${VERSION}"; then
-    echo "VERSION is not set"
-    exit 1
-fi
 
 make metadata.json
-mkdir -p test/var/cache/uniget
-cp metadata.json test/var/cache/uniget/metadata.json
 
-uniget --prefix=test generate --base registry.gitlab.com/uniget-org/images/ubuntu:24.04 "${TOOL}@${VERSION}" \
-| docker build --tag test --load -
-
-docker run --interactive --rm --env TOOL --env VERSION --volume "${PWD}/metadata.json:/var/cache/uniget/metadata.json" test bash -o errexit <<EOF
+docker run \
+    --interactive \
+    --rm \
+    --env TOOL \
+    --volume "${PWD}/metadata.json:/var/cache/uniget/metadata.json" \
+    registry.gitlab.com/uniget-org/images/ubuntu:24.04 \
+        bash -o errexit <<EOF
 curl -sLf https://github.com/uniget-org/cli/releases/latest/download/uniget_linux_$(uname -m).tar.gz \
 | tar -xzC /usr/local/bin uniget
-mv /usr/local/var/lib/uniget /var/lib/
+uniget install ${TOOL}
 uniget list --installed
 uniget healthcheck "${TOOL}"
 uniget version "${TOOL}"
