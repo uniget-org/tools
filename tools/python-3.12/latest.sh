19b6c564fe0e4a332f8d64a15cf41882d6143679
Nicholas Dille <nicholas.dille@mailbox.org>
2025-04-11 14:45:14 +0200

    fix(python-3.12): Script can now run standalone

--- tools/python-3.12/latest.sh
+++ tools/python-3.12/latest.sh
@@ -5,7 +5,8 @@ curl --silent --show-error --location --fail \
     --url "https://api.github.com/repos/indygreg/python-build-standalone/releases/latest" \
 | jq --raw-output '.assets[].name' \
 | grep -E -- '-x86_64-unknown-linux-musl-install_only.tar.gz$' \
-| sed -E 's/^cpython-([^-]+)-.+$/\\1/' \
-| grep -P '^3\\.12\\.' \
+| sed -E 's/^cpython-([^-]+)-.+$/\1/' \
+| grep -P '^3\.12\.' \
 | sort -Vr \
-| head -n 1
+| head -n 1 \
+| tr '+' '-'
