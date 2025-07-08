83117b8ae0e4d216931ada14070441247f2240ca
Nicholas Dille <nicholas.dille@mailbox.org>
2025-07-08 11:39:06 +0200

    Improved PR analysis

--- scripts/pr-stats.sh
+++ scripts/pr-stats.sh
@@ -4,27 +4,31 @@ set -o errexit -o pipefail
 function process_prs() {
     jq '
         [
-            .[] |
-            select(.merged_at != null and .created_at != null) |
+            .[] as $pr |
+            $pr.labels[] | select(.name == "type/renovate") |
+            select($pr.merged_at != null and $pr.created_at != null) |
             {
-                "repo": .base.repo.full_name,
-                "number": "\(.number)",
-                "link": "\(.base.repo.full_name)#\(.number)",
-                "title": .title,
-                "createdAt": .created_at,
-                "createdAtDay": (.created_at | fromdateiso8601 | strftime("%Y-%m-%d")),
-                "mergedAt": .merged_at,
-                "openMilliseconds": ( ( (.merged_at | fromdateiso8601) - (.created_at | fromdateiso8601) ) / 1000 )
+                "repo": $pr.base.repo.full_name,
+                "number": "\($pr.number)",
+                "link": "\($pr.base.repo.full_name)#\(.number)",
+                "title": $pr.title,
+                "createdAt": $pr.created_at,
+                "createdAtDay": ($pr.created_at | fromdateiso8601 | strftime("%Y-%m-%d")),
+                "mergedAt": $pr.merged_at,
+                "openSeconds": ( ( ($pr.merged_at | fromdateiso8601) - ($pr.created_at | fromdateiso8601) ) / 1000 )
             }
         ]
     '
 }
 
+mkdir -p data
 if ! test -f data/prs.json; then
     echo "Fetching PRs from uniget-org/tools"
-    gh api --paginate "repos/uniget-org/tools/pulls?state=closed" >data/prs.json
+    gh api --paginate "repos/uniget-org/tools/pulls?state=closed" \
+    | process_prs \
+    >data/prs.json
 fi
-if ! test -f data/prs-old.json.gz; then
+if ! test -f data/prs-old.json; then
     echo "Fetching PRs from nicholasdille/docker-setup"
     gh api --paginate "repos/nicholasdille/docker-setup/pulls?state=closed" \
     | jq '
@@ -37,31 +41,28 @@ if ! test -f data/prs-old.json.gz; then
             )
         ]
     ' \
+    | process_prs \
     >data/prs-old.json
-else
-    gunzip data/prs-old.json.gz
 fi
-echo "Compiling reduced-prs.json"
-cat data/prs-old.json data/prs.json \
-| jq --slurp '
-    [
-        .[][] as $pr |
-            $pr.labels[] | select(.name == "type/renovate") | $pr
-    ]
-' \
-| process_prs \
->data/reduced-prs.json
-gzip data/prs-old.json
 
-echo "Total PRs"
-cat data/reduced-prs.json \
+cat data/prs.json data/prs-old.json \
+| jq --slurp '.[0] + .[1]' \
+>data/combined-prs.json
+
+echo -n "Total PRs: "
+cat data/combined-prs.json \
 | jq 'length'
+
+echo -n "First PR: "
+cat data/combined-prs.json \
+| jq -r 'sort_by(.createdAt) | .[0] | .createdAt'
+
 echo "Analyzing PRs by day"
-cat data/reduced-prs.json \
+cat data/combined-prs.json \
 | jq 'group_by(.createdAtDay)' \
 | jq ' [ .[] | { "date": .[0].createdAtDay, "count": (. | length) } ]' \
 | mlr --j2x stats1 -a min,mean,max -f count
 
 echo "Analyzing duration of PRs"
-cat data/reduced-prs.json \
-| mlr --j2x stats1 -a min,median,mean,max,p90,p95,p98 -f openMilliseconds
+cat data/combined-prs.json \
+| mlr --j2x stats1 -a min,median,mean,p90,p95,p98,max -f openSeconds
