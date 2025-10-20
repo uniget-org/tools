5279872d5b1e73b23f2c0e52687dde50edf3879d
Nicholas Dille <nicholas.dille@mailbox.org>
2025-10-20 15:28:04 +0200

    feat: Smoke test now more resilient

--- scripts/test.sh
+++ scripts/test.sh
@@ -7,18 +7,20 @@ if test -z "${TOOL}"; then
 fi
 
 make metadata.json
+make "${TOOL}--tar"
 
 docker run \
     --interactive \
     --rm \
     --env TOOL \
     --volume "${PWD}/metadata.json:/var/cache/uniget/metadata.json" \
+    --volume ${PWD}/tools/${TOOL}/image.tar:/tmp/${TOOL}.tar \
     registry.gitlab.com/uniget-org/images/ubuntu:24.04 \
         bash -o errexit <<EOF
 curl -sLf https://github.com/uniget-org/cli/releases/latest/download/uniget_linux_$(uname -m).tar.gz \
 | tar -xzC /usr/local/bin uniget
-uniget install ${TOOL}
-uniget list --installed
-uniget healthcheck "${TOOL}"
-uniget version "${TOOL}"
+uniget --prefix=test install --path-to-tar-mappings=${TOOL}=/tmp/${TOOL}.tar ${TOOL}
+uniget --prefix=test list --installed
+uniget --prefix=test healthcheck "${TOOL}"
+uniget --prefix=test version "${TOOL}"
 EOF
