a645d00645634f7bdf98456c4d05a01f2b073952
Nicholas Dille <nicholas.dille@mailbox.org>
2025-10-20 21:32:51 +0200

    fix: Run test on new tools

--- scripts/test.sh
+++ scripts/test.sh
@@ -13,14 +13,14 @@ docker run \
     --interactive \
     --rm \
     --env TOOL \
-    --volume "${PWD}/metadata.json:/var/cache/uniget/metadata.json" \
+    --volume "${PWD}/metadata.json:/tmp/test/var/cache/uniget/metadata.json" \
     --volume ${PWD}/tools/${TOOL}/image.tar:/tmp/${TOOL}.tar \
     registry.gitlab.com/uniget-org/images/ubuntu:24.04 \
         bash -o errexit <<EOF
 curl -sLf https://github.com/uniget-org/cli/releases/latest/download/uniget_linux_$(uname -m).tar.gz \
 | tar -xzC /usr/local/bin uniget
-uniget --prefix=test install --path-to-tar-mappings=${TOOL}=/tmp/${TOOL}.tar ${TOOL}
-uniget --prefix=test list --installed
-uniget --prefix=test healthcheck "${TOOL}"
-uniget --prefix=test version "${TOOL}"
+uniget --prefix=/tmp/test install --path-to-tar-mappings=${TOOL}=/tmp/${TOOL}.tar ${TOOL}
+uniget --prefix=/tmp/test list --installed
+uniget --prefix=/tmp/test healthcheck "${TOOL}"
+uniget --prefix=/tmp/test version "${TOOL}"
 EOF
